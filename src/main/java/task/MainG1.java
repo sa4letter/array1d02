8cca3b930f6a3eef58d3a08e53ed2a586b4f22d8
vano0713 <valery.novoselov@NetCracker.com>
2019-10-16 09:58:56 +0300

    The task was completed

--- src/main/java/task/MainG1.java
+++ src/main/java/task/MainG1.java
@@ -1,46 +1,33 @@
 package task;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainG1 {
   private static int[][] sMatrix;
   private static int[] pathMatrix;
 
-  private static void findAndPutNextAbove(int base, int current, int except) {
-    for (int k = current; k < sMatrix[current].length; k++) {
-      //System.out.println("kA="+k);
-      if (sMatrix[current][k] == 1 && k != except) {
+
+  private static void findAndPutNext(int current, int except) {
+
+    for (int k = 0; k < sMatrix[current].length; k++) {
+      if (sMatrix[current][k] == 1 && pathMatrix[k] == 0) {
         pathMatrix[k] = 1;
-        System.out.println(String.format("Above findAndPutNext(%d, %d, %d); ->%s", base, k, k, Arrays.toString(sMatrix[k])));
-        findAndPutNextAbove(base, k, k);
+        findAndPutNext(k, current);
       }
-
-//      System.out.println("kB="+k);
-//      if(k-1 >= 0 && k != except && sMatrix[current][k-1] == 1){
-//        pathMatrix[k-1] = 1;
-//        System.out.println(String.format("Below findAndPutNext(%d, %d, %d); ->%s", base, k-1, k, Arrays.toString(sMatrix[k-1])));
-//        findAndPutNextAbove(base, k-1, k);
-//      }
-    }
-  }
-
-  private static void findAndPutNextBelow(int base, int cCol, int except) {
-    for (int k = cCol; k < sMatrix[cCol].length; k++) {
-      if (sMatrix[cCol][k] == 1) {
-        if(k>0 && cCol>0 && sMatrix[k][cCol-1] == 1){
-          System.out.println(String.format("Below findAndPutNext(%d, %d, %d); ->%s", base, k-1, k, Arrays.toString(sMatrix[k-1])));
-        }
-        //pathMatrix[k-1] = 1;
-        System.out.println("++");//Below findAndPutNext(%d, %d, %d); ->%s", cRow, k-1, k, Arrays.toString(sMatrix[k-1])));
-        //findAndPutNextBelow(cRow, k-1, k);
-      }
-
     }
   }
 
   private static boolean canWin(int leap, int[] game) {
     // Return true if you can win the game; otherwise, return false.
-    System.out.println("Original: " + Arrays.toString(game) + " leap = "+ leap);
+    //System.out.println("Original: " + Arrays.toString(game));// + " leap = "+ leap);
     // fast check
     if(leap >= game.length) return true;
 
@@ -65,40 +52,70 @@ public class MainG1 {
         //III - leap
         if (i + leap < game.length && game[i + leap] == 0 && leap > 0) {
           sMatrix[i][i + leap] = 1; //row
-          sMatrix[i + leap][i] = 1; //column
         }
       }
     }
 
-    System.out.println("sMatrix");
-    for (int i = 0; i < game.length; i++) {
-      System.out.println("i=" + i + "->" + Arrays.toString(sMatrix[i]));
-    }
-
-    findAndPutNextAbove(0, 0, 0);
-    System.out.println("pathMatrix: "+Arrays.toString(pathMatrix));
-
-    findAndPutNextBelow(0, 0, 0);
-    System.out.println("pathMatrix: "+Arrays.toString(pathMatrix));
+    findAndPutNext(0, 0);
 
     for(int z=pathMatrix.length-1; z>=0; z--){
-
-      if(z<pathMatrix.length-leap) return false;
+      if(z<pathMatrix.length-(leap==0?1:leap)) return false;
       if(pathMatrix[z]==1) return true;
     }
 
     return false;
-    // else return false;
-
   }
 
   public static void main(String[] args) {
-               // 0  1  2  3  4  5  6  7  8  9 10  11 12
+
+    Pattern parameters = Pattern.compile("^(\\d{1,4}),(\\d{1,4})$");
+    String file = "tc3.data";
+    Path inFile = Paths.get(file);
+
+    try(BufferedReader reader = Files.newBufferedReader(inFile, Charset.forName("UTF-8"))
+    ){
+      String row;
+      int currentLeap;
+      int arraySize;
+
+      while((row = reader.readLine())!= null){
+        Matcher matcher = parameters.matcher(row);
+        if(matcher.find()){
+          currentLeap = Integer.parseInt(matcher.group(2));
+          arraySize = Integer.parseInt(matcher.group(1));
+
+          int[] currentArray = new int[arraySize];
+          if((row = reader.readLine())!= null){
+            int i = 0;
+            for(String s : row.split(",")){
+              currentArray[i++]=Integer.parseInt(s);
+            }
+          }else{
+            break;
+          }
+
+          System.out.println((canWin(currentLeap, currentArray)?"YES":"NO")/*+" "+matcher.group(0)*/);
+        }
+      }
+
+    }catch(IOException ioe){
+      System.err.format("Try to work with file %s\n", ioe);
+    }
+
+                 // 0  1  2  3  4  5  6  7  8  9 10  11 12
     int[] game = //{0, 1, 0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0};
                  //{0, 1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0};
-                 {0, 1, 0, 0, 1, 0, 1, 1, 0}; //3 backward
-                 //{0, 0, 0, 0, 0};
-    int leap = 3;
-    System.out.println("Res=" + canWin(leap, game));
+                 //{0, 1, 0, 0, 1, 0, 1, 1, 0}; //3 backward
+                 //{0, 1, 1, 0, 0, 1, 0, 0, 0, 1};// 3 backward
+                 //{0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 0};//5
+                 //{0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0};//5
+                 //{0, 0, 1, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 0, 1, 1, 1, 1, 0};//5
+                 //{0, 0, 0, 0, 0}; //3
+                 //TC3
+                 //{0, 0, 0, 0, 0, 1, 0, 1, 0};//41
+                 //TC4
+                 {0, 0}; //2,0
+    int leap = 0;//11;
+    //System.out.println("Res=" + canWin(leap, game));
   }
 }
